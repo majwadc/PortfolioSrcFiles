d38108038a4f62521de3344bf2dd3e7d4e17a294
mohammedajwad <majwadc@gmail.com>
2022-08-20 13:10:50 +0400

    google seo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Work from './components/Work'
 import Contact from './components/Contact'
+
 function App() {
+  <meta name="google-site-verification" content="lzN1Kq1p-hyxB1seRIXlDZ-zSAIEW3gQeqGbIWQimHo" />
   return (
     <div> 
       <Navbar/>
